53a65385adf83d74d7a8f6a1ddfb89c044c83647
Brudja <n0669378587@gmail.com>
2022-09-16 14:43:15 +0300

    добавляет решение 9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const body = document.querySelector("body")
+const btnStart = body.querySelector('button[data-start]');
+const btnStop = body.querySelector('button[data-stop]');
 
+// console.log('btnStart', btnStart);
+// console.log('btnStop', btnStop);
+
+btnStart.addEventListener("click", clickStart);
+btnStop.addEventListener("click", clickStop);
+
+btnStop.disabled = true;
+let timerColorChenge;
+
+function clickStart(){
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerColorChenge = setInterval(() => {
+        const bgcBody = getRandomHexColor();
+        body.style.background = bgcBody;
+    }, 1000)
+};
+
+function clickStop(){
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerColorChenge)
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
