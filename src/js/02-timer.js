895b37910a4d0866bd05884d61192f1c48011dc0
Brudja <n0669378587@gmail.com>
2022-09-20 16:31:37 +0300

    Update 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,86 +1,3 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import Notiflix from 'notiflix';
-
-
-// const body = document.querySelector("body")
-// const input = body.querySelector("#datetime-picker");
-// const btnStart = body.querySelector("button[data-start]");
-// const daysTimer = body.querySelector('[data-days]');
-// const hoursTimer = body.querySelector('[data-hours]');
-// const minutesTimer = body.querySelector('[data-minutes]');
-// const secondsTimer = body.querySelector('[data-seconds]');
-
-// let selectedDay = new Date();
-// btnStart.disabled = true;
-
-// const options = {
-//     enableTime: true,
-//     time_24hr: true,
-//     defaultDate: new Date(),
-//     minuteIncrement: 1,
-//     onClose(selectedDates) {
-//       selectedDay = selectedDates[0];
-//       console.log(selectedDates[0]);
-//     },
-//   };
-
-//   window.addEventListener('load', reloadP);
-//   function reloadP() {}
-//   function addLeadingZero(value) {
-//     return value.toString().padStart(2, '0');
-//   }
-//   btnStart.addEventListener('click', btnClick);
-//   function timerBig({ days, hours, minutes, seconds }) {
-//     daysTimer.textContent = addLeadingZero(`${days}`);
-//     hoursTimer.textContent = addLeadingZero(`${hours}`);
-//     minutesTimer.textContent = addLeadingZero(`${minutes}`);
-//     secondsTimer.textContent = addLeadingZero(`${seconds}`);
-//   }
-
-
-//   function btnClick(event) {
-//     intervalId = setInterval(() => {
-//       const msResult = selectedDay.getTime() - Date.now();
-//       const timer = convertMs(msResult);
-//       const { days, hours, minutes, seconds } = timer;
-//       if (msResult < 1000 || pageReload()) {
-//         clearInterval(intervalId);
-//         return;
-//       }
-//       timerBig(timer);
-//     }, 1000);
-//   }
-//   input.addEventListener('change', onInputClick);
-//   function onInputClick(event) {
-//     const selectedTime = new Date(event.target.value);
-//     if (selectedTime.getTime() < Date.now()) {
-//       return Notiflix.Notify.failure('Please choose a date in the future');
-//     }
-//     btnStart.removeAttribute('disabled');
-//   }
-
-// flatpickr(input, options);
-
-// function convertMs(ms) {
-
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = Math.floor(ms / day);
-
-//   const hours = Math.floor((ms % day) / hour);
-
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-//   return { days, hours, minutes, seconds };
-// }
-
-
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
@@ -121,6 +38,7 @@ function onBtnClick(event) {
     timerBig(timer);
   }, 1000);
 }
+
 input.addEventListener('change', onInputClick);
 function onInputClick(event) {
   const selectedTime = new Date(event.target.value);
